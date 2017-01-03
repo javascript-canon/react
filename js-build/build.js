e12996a8fe89a31db19e3c5ed74ac26fb9582b29
Kai Gittens <kai.gittens@gmail.com>
2017-01-03 16:14:34 -0500

    update "js-build/build.js"...this will be removed or renamed soon

--- js-build/build.js
+++ js-build/build.js
@@ -1,13 +1,1 @@
-import React from 'react'
-import { render } from 'react-dom'
-
-window.React = React;
-/*
-render(
-  // build the component & load into the
-  <App />,
-  document.getElementById('react-container')
-)*/
-
-
 console.log("foobars");
