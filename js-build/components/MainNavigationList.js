e8cabca3a7d0baca0f7949235bf8f7a17fa52fd8
Kai Gittens <kai.gittens@gmail.com>
2017-01-06 15:36:27 -0500

    update comments & whitespace

--- js-build/components/MainNavigationList.js
+++ js-build/components/MainNavigationList.js
@@ -1,9 +1,16 @@
 // use strict mode
 "use strict";
 
+// Make the list item component available
 import { MainNavigationListItem } from './MainNavigationListItem';
 
+/* Create a list item component and point to the "resourceTypes"
+ * object in "react-build-out.js" in the parameter
+ */
 export const MainNavigationList = ({resourceTypes}) => (
+  /* Use Array.prototype.map() to loop over the resource type object
+   * and place object properties in the list item component
+   */
     <ul className="nav__list">
       {resourceTypes.map((getTypes, i) =>
         <MainNavigationListItem key={i}
