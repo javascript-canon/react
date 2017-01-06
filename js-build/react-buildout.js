f05459f68e8307754cc11557b4a61928fec05dc1
Kai Gittens <kai.gittens@gmail.com>
2017-01-06 15:19:38 -0500

    remove uneeded import, comments and render shit, add new import, window call & rendermethod

--- js-build/react-buildout.js
+++ js-build/react-buildout.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { hello, goodbye } from './lib'
+import { MainNavigationList } from './components/MainNavigationList';
 
-// AJAX in the resources API with Axios after the component renders
-// componentDidMount: function() {
-//   var _this = this;
-//   this.serverRequest =
-//     axios.get('http://localhost:3000/api/resources')
-//       .then(function (response) {
-//         console.log(response.data);
-//       })
-//       .catch(function (error) {
-//         console.log(error);
-//       });
-//   },
-
+window.React = React;
   render(
-    <div>
-      {hello}
-      {goodbye}
-    </div>,
-  document.getElementById('navigation')
+    <MainNavigationList resourceTypes = {
+      [
+        {
+          type: "show all types"
+        },
+        {
+          type: "books"
+        },
+        {
+          type: "websites"
+        },
+        {
+          type: "classes"
+        }
+      ]
+    } />,
+  document.getElementById('nav__react-target')
 )
-
