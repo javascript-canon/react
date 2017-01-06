97be99bb750823eb1c012dd0a8d90de4e8fadbb2
Kai Gittens <kai.gittens@gmail.com>
2017-01-06 15:39:09 -0500

    use the JSX spread operator to populate list items

--- js-build/components/MainNavigationList.js
+++ js-build/components/MainNavigationList.js
@@ -14,7 +14,7 @@ export const MainNavigationList = ({resourceTypes}) => (
     <ul className="nav__list">
       {resourceTypes.map((getTypes, i) =>
         <MainNavigationListItem key={i}
-                                type={getTypes.type} />
+                                {...getTypes} />
       )}
     </ul>
   )
