11bf7dcb8bef465ef0308b16c449e53066e6658e
Kai Gittens <kai.gittens@gmail.com>
2017-01-06 22:09:52 -0500

    update core <nav>

--- js-build/react-buildout.js
+++ js-build/react-buildout.js
@@ -8,22 +8,23 @@ import { MainNavigationList } from './components/MainNavigationList';
 // Make sure React is attached to the window object to avoid bugs
 window.React = React;
 
-  render(
-    <MainNavigationList resourceTypes = {
-      [
-        {
-          type: "show all types"
-        },
-        {
-          type: "books"
-        },
-        {
-          type: "websites"
-        },
-        {
-          type: "classes"
-        }
-      ]
-    } />,
+// Build the <nav>
+render(
+  <MainNavigationList resourceTypes = {
+    [
+      {
+        type: "show all types"
+      },
+      {
+        type: "books"
+      },
+      {
+        type: "websites"
+      },
+      {
+        type: "classes"
+      }
+    ]
+  } />,
   document.getElementById('nav__react-target')
 )
