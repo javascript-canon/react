e12a6ee0cc81749cc86cd46abd90ff8e8ed8c318
Kai Gittens <kai.gittens@gmail.com>
2017-01-08 21:40:50 -0500

    add comments, update whitespace

--- js-build/components/NavListContainer.js
+++ js-build/components/NavListContainer.js
@@ -1,12 +1,24 @@
 // use strict mode
 "use strict";
 
+// Import React
 import React from 'React';
-import { NavList } from './NavList';
+
+// Import jQuery
 import $ from 'jquery';
+
+// Import core jQuery
 import _ from 'underscore';
 
+// Import the NavList child component
+import { NavList } from './NavList';
 
+/* An implementation of the React Container Components design pattern,
+ * where a parent component fetches data via AJAX and passes it down
+ * to a child component (read more at: http://bit.ly/2jiTEdg). The
+ * data being fetched contains duplicates...they're removed by
+ * underscore before being passed down to the child component.
+ */
 export class NavListContainer extends React.Component {
   constructor() {
     super();
