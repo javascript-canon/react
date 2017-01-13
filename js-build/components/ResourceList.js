ee228d71f0e1ce275e33843aff162f6dcf4bfe36
Kai Gittens <kai.gittens@gmail.com>
2017-01-12 21:48:14 -0500

    get single resource components to load onto the page

--- js-build/components/ResourceList.js
+++ js-build/components/ResourceList.js
@@ -15,9 +15,13 @@ export class ResourceList extends Component {
     </div>;
   }
 
-  renderResources({author}) {
-    return <article className="single-resource">
-      <a className="nav__button">{author}</a>
+  renderResources({i, title, author, image, link, href}) {
+    return <article className="single-resource" key={i}>
+      <h2 className="single-resource__header">{title}<span className="single-resource__author"> &nbsp;by {author}</span>
+      </h2>
+      <img className="single-resource__book-image" src={'/img/book-images/' + image} />
+      <a className="single-resource__button js-book" href={link}>Get This!</a>
+      <a className="single-resource__button" href={'/react/' + href}>Why it's good?</a>
     </article>;
   }
 }
