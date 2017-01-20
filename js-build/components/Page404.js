6873c974638ebcb2031a7f9f76fcb3376b033cdb
Kai Gittens <kai.gittens@gmail.com>
2017-01-20 16:21:30 -0500

    add 404 back

--- js-build/components/Page404.js
+++ js-build/components/Page404.js
@@ -1,7 +1,7 @@
 // A Stateless component
 export const Page404 = () =>
-  <div className="page-404" itemProp="mainContentOfPage" itemType="http://schema.org/CreativeWork">
-      <h1 className="header__title">404 error not found</h1>
+  <div className="page-404" id="foo" itemProp="mainContentOfPage" itemType="http://schema.org/CreativeWork">
+      <h1 className="header__title">404: error not found</h1>
       <img src="/img/404-image-one.png" className="page-404__image page-404--image-one" alt="Image one of the JavaScript Canon's 404 page" />
       <img src="/img/404-image-two.png" className="page-404__image page-404--image-two" alt="Image one of the JavaScript Canon's 404 page" />
       <p className="page-404__content">
