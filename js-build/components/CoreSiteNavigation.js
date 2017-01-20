934fe1adce5a05a2d9a33143794ec4305dda3ab8
Kai Gittens <kai.gittens@gmail.com>
2017-01-20 15:07:42 -0500

    bump react-router and babel-related dependencies

--- js-build/components/CoreSiteNavigation.js
+++ js-build/components/CoreSiteNavigation.js
@@ -17,10 +17,12 @@ export const CoreSiteNavigation = () => (
 
   <ul className="nav__list">
     <li className="nav__list-item-home nav--link-top-padding">
-      <a href="/" className="nav--homepage-link nav__button-home">return to main site</a>
+      <a href="/" className="nav--homepage-link nav__button-home">return to main sites</a>
     </li>
     <li className="nav__list-item-home">
       <a href="/about" className="nav--homepage-link nav__button-home">about</a>
     </li>
   </ul>
   )
+
+
