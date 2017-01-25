a815e4e596c9785536e1116f3e967c48815bfc52
Kai Gittens <kai.gittens@gmail.com>
2017-01-25 11:31:23 -0500

    remove commented-out code & test router

--- js-build/index.js
+++ js-build/index.js
@@ -6,6 +6,7 @@ import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router'
 import { App } from './components/App';
 import { NavListContainer } from './components/NavListContainer';
+import { SingleResource } from './components/SingleResource';
 import { Page404 } from './components/Page404';
 
 // Make sure React is attached to the window object to avoid bugs
@@ -17,17 +18,13 @@ render(
   document.getElementById('nav__react-target')
 )
 
-// Build the core <App> container and place it in the main content area
-// render(
-//   <App />,
-//   document.getElementById('container__react-target')
-// )
 /* Build all the resources and place them on the page via a bound
  * router
  */
 render(
   <Router history={hashHistory}>
     <Route path="/" component={App} />
+    <Route path="/foo" component={SingleResource} />
     <Route path="*" component={Page404} />
   </Router>,
   document.getElementById('container__react-target')
